f63c6bfcf782ee457ff759d1ce98e1361886fe0f
“Mark <mkanko66@gmail.com>
2020-01-05 21:56:51 -0800

    add index and create actions to NotesController

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+
+    def index
+        note = Note.all 
+        render json: NoteSerializer.new(note)
+    end
+    
+    def create 
+        if !(note = Note.find_by(user_id: params[:user_id], video_id: params[:video_id]))
+            note = Note.create(title: params[:title], date: params[:date], 
+            user_id: params[:user_id], video_id: params[:video_id])
+            
+            render json: NoteSerializer.new(note)
+        else 
+            redirect_to note 
+        end
+    end
+
+
 end
