25f9d6cb82883846de17797321b283107a8d5600
“Mark <mkanko66@gmail.com>
2020-01-05 23:36:21 -0800

    add NoteList.js file to client/src/components/notes, add import React, class component NoteList, export default

---
+++ client/src/components/notes/NoteList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class NoteList extends React.Component {
+    render() {
+        return (
+            <div>
+                Note List 
+            </div>
+        )
+    }
+} 
+
+
+export default NoteList
