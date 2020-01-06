35bd19676c4e839eb3ec510474ec8b308da2596f
“Mark <mkanko66@gmail.com>
2020-01-05 23:45:28 -0800

    add import statements for SignIn, NoteList, and VideoList components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-
+import SignIn from './components/SignIn'
+import NoteList from './components/notes/NoteList'
+import VideoShow from './components/videos/VideoShow'
 
 
 const App = () => {
