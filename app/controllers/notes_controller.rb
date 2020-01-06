0cb564512d522a63b2a14221ce2a8afadc5311a1
“Mark <mkanko66@gmail.com>
2020-01-05 22:04:11 -0800

    add show and update actions to NotesController

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,11 @@
 class NotesController < ApplicationController
 
     def index
-        note = Note.all 
+        note = Note.all
+
         render json: NoteSerializer.new(note)
-    end
-    
+    end   
+#                                                            # content: params[:content]
     def create 
         if !(note = Note.find_by(user_id: params[:user_id], video_id: params[:video_id]))
             note = Note.create(title: params[:title], date: params[:date], 
@@ -15,6 +16,23 @@ class NotesController < ApplicationController
             redirect_to note 
         end
     end
+
+    def show
+        if !(note = Note.find_by(user_id: params[:user_id], video_id: params[:video_id]))
+            note = Note.find(params[:id])
+        end 
+
+        render json: NoteSerializer.new(note)
+    end
+    
+    def update
+        note = Note.find_by(user_id: params[:user_id], video_id: params[:video_id])
+        note.update(content: params[:content])
+
+        render json: NoteSerializer.new(note)
+    end 
+
+    
 
 
 end
