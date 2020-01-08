d5462db52ddf2378c7dcbe359e49277cbc7aa2ad
“Mark <mkanko66@gmail.com>
2020-01-08 15:38:25 -0800

    add onAuthChange() to GoogleAuth.js

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -18,6 +18,14 @@ class GoogleAuth extends React.Component {
         })
     }
 
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn(this.auth.currentUser.get().getId())
+        } else {
+            this.props.signOut()
+        }
+    }
+
     onSignInClick = () => {
         this.auth.signIn()
     }
