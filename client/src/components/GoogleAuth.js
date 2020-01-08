01fe9a67fa656f5364c1b60f7821888a713cf145
“Mark <mkanko66@gmail.com>
2020-01-07 20:11:04 -0800

    add componentDidMount method with google authentication request

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 
 class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '336044228262-0uk6f92mpfsu7e9olrd7ru4crna94jac.apps.googleusercontent.com',
+                scope: 'email'  
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.onAuthChange(this.auth.isSignedIn.get())
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
+        })
+    }
+
     render() {
         return (
            <div>
