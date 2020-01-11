9fbf98ce00a3001223784f31833d3ea11fcad1b4
“Mark <mkanko66@gmail.com>
2020-01-11 12:34:28 -0800

    add class based component VideosContainer to VideosContainer.js, import - react from react, connect from react-redux, SearchBar from videos/SearchBar

---
+++ client/src/containers/VideosContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import SearchBar from '../videos/SearchBar'
+
+class VideosContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+}
+
+
+export default VideosContainer 
