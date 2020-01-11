2623dddd161a34774ed2e46b871b39f2ee2f1233
“Mark <mkanko66@gmail.com>
2020-01-11 14:41:59 -0800

    change back to import BrowserRouter and BrowserRouter component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import SignIn from './components/SignIn'
 import NoteList from './components/notes/NoteList'
-import VideoShow from './components/VideoShow' 
+import VideoShow from './components/VideoShow'
 
 
 const App = () => {
     return (
         <div className="ui container">
-            <Router history={history}>
+            <BrowserRouter>
                 <div>                
                     <Route path="/" exact component={SignIn} />
                     <Route path="/notes/list" exact component={NoteList} />
                     <Route path="/videos/show" exact component={VideoShow} />
                 </div>
-            </Router>
+            </BrowserRouter>
         </div>
     )
 }
