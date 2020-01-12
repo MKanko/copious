c694895a3335e7ec538eab9888b9947ea141c09f
“Mark <mkanko66@gmail.com>
2020-01-12 14:01:47 -0800

    add export default axios.create to youtube.js

--- client/src/apis/youtube.js
+++ client/src/apis/youtube.js
@@ -1,1 +1,14 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyCqSZoBxSy8VN3SAeCjMC0KFPDj8gfxwo4'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 10,
+        key: KEY 
+    }
+}) 
+
+
