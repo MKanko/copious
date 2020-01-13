23224527a33ff61131a184ea554cb72200227f82
“Mark <mkanko66@gmail.com>
2020-01-12 16:27:58 -0800

    import VideoItem in VideoList.js, add code to map over videos array and return VideoItem component

--- client/src/components/videos/VideoList.js
+++ client/src/components/videos/VideoList.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import VideoItem from './VideoItem'
+
 
 
 const VideoList = (props) => {
+    const renderList = props.videos.map((video) => {
+        return <VideoItem />
+    })
     return (
         <div>
-            VideoList  
+            {renderList}  
         </div>
     )
 }
