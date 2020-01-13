9882856121ec7e8a304319c5622ada06d93e01bc
“Mark <mkanko66@gmail.com>
2020-01-12 18:03:47 -0800

    add iframe for video player w/semantic styling, and helper var videoSrc for iframe src property

--- client/src/components/videos/VideoDetail.js
+++ client/src/components/videos/VideoDetail.js
@@ -5,8 +5,14 @@ const VideoDetail = (props) => {
     if (!props.video) {
         return <div>Loading...</div>
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`
+    
     return (
         <div>
+            <div className="ui imbed">
+                <iframe src={videoSrc} />
+            </div>
             <div className="ui segment">
                 <h4 className="header">{props.video.snippet.title}</h4>
                 <p>{props.video.snippet.description}</p>
--- client/src/components/videos/VideoList.js
+++ client/src/components/videos/VideoList.js
@@ -4,8 +4,8 @@ import VideoItem from './VideoItem'
 
 
 const VideoList = (props) => {
-    const renderList = props.videos.map((video) => {
-        return <VideoItem onVideoSelect={props.onVideoSelect} video={video} />
+    const renderList = props.videos.map((video, index) => {
+        return <VideoItem onVideoSelect={props.onVideoSelect} video={video} key={index} />
     })
     return (
         <div className="ui relaxed divided list">
