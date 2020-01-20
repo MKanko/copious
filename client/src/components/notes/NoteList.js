074265e9e341bc7aff01824e66a774e0c255adb2
“Mark <mkanko66@gmail.com>
2020-01-19 19:04:56 -0800

    create NoteList.js on client/src/components/notes, add fuctional Notelist component import react, export default

---
+++ client/src/components/notes/NoteList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const NoteList = () => {
+    return (
+        <div>
+            Note List 
+        </div>
+    )
+}
+
+
+export default NoteList 
--- client/src/containers/NotesContainer.js
+++ client/src/containers/NotesContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import { fetchNotes } from '../actions'
 import NoteHeader from '../components/notes/NoteHeader'
+import NoteList from '../components/notes/NoteList'
 
 
 // actions sending get requests to find note,  post requests to create or update a note are actions in the actions file
@@ -17,7 +18,8 @@ class NotesContainer extends React.Component {
     render() {
         return (
             <div className="ui container">
-              <NoteHeader />                
+                <NoteHeader /> 
+                <NoteList notes={this.props.notes} /> 
             </div>
         )
     }
