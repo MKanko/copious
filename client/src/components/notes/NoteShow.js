7ce7e66ff3fd0f420841b6b0dbda869730a6a3d8
“Mark <mkanko66@gmail.com>
2020-01-25 10:38:01 -0800

    create NoteShow.js, add class based NoteShow component

---
+++ client/src/components/notes/NoteShow.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import { getNote } from '../../actions'
+import NoteHeader from './NoteHeader'
+import NoteEdit from './NoteEdit'
+
+class NoteShow extends React.Component {
+
+    componentDidMount() {
+        this.props.getNote(this.props.match.params.id)
+    }
+
+    render() {
+        let editForm
+        let title
+        if (this.props.note && this.props.note.data) {    
+            editForm = <NoteEdit 
+                            note={this.props.note} 
+                            userId={this.props.note.data.attributes.userId} 
+                            videoId={this.props.note.data.attributes.videoId} 
+                        />
+            title = this.props.note.data.attributes.title
+        }
+
+        return (
+            <div className="ui container">
+                <NoteHeader />
+                <div className="ui grey segment">
+                    <div className="ui form">
+                        <div className="field">
+                            <Link to={{
+                                pathname: '/videos/show',
+                                state: {
+                                    title: title
+                                }
+                            }}>{title}</Link>
+                            {editForm}
+                        </div>
+                    </div>                          
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    console.log(state)
+    return { note: state.notes.note }
+}
+
+
+export default connect(mapStateToProps, { getNote })(NoteShow)
+
+// console.log(props)
+//a function that checks props to see if note exists to determine which button to display
+// the logic involved in searching db for note will be in notesContainers and passed to noteDetail as props
