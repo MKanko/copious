f3cb47b79490fbe795848747845a4a2812890f9f
“Mark <mkanko66@gmail.com>
2020-01-25 18:58:21 -0800

    add css styling to ui container div and ui segment div

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,17 @@ import NoteShow from './components/notes/NoteShow'
 
 const App = () => {
     return (
-        <div>
-            <Router history={history}>
-                <Switch>                
-                    <Route path="/" exact component={SignIn} />
-                    <Route path="/notes/list" exact component={NotesContainer} />
-                    <Route path="/videos/show" exact component={VideosContainer} />
-                    <Route path="/notes/:id" exact component={NoteShow} />
-                </Switch>
-            </Router>
+        <div className="ui container" style={{ padding: '50px'}}>
+            <div className="ui segment" style={{ backgroundColor: '#D1D1D1' }}>
+                <Router history={history}>
+                    <Switch>                
+                        <Route path="/" exact component={SignIn} />
+                        <Route path="/notes/list" exact component={NotesContainer} />
+                        <Route path="/videos/show" exact component={VideosContainer} />
+                        <Route path="/notes/:id" exact component={NoteShow} />
+                    </Switch>
+                </Router>
+            </div>
         </div>
     )
 }
