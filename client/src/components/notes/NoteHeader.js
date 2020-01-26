1ab06109be5838cb6e234c0a5719d0e0f58cf613
“Mark <mkanko66@gmail.com>
2020-01-25 18:53:48 -0800

    change h5 inner text

--- client/src/components/notes/NoteHeader.js
+++ client/src/components/notes/NoteHeader.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import GoogleAuth from '../GoogleAuth'
+
 
 const NoteHeader = () => {
     return (
         <div className="ui secondary pointing menu">
-            <h5>Notes</h5>
+            <h5>Your Notes</h5>
             <div className="right menu">
                 <Link to="/videos/show" className="item">
-                    Tutorials/Sign Out 
+                    Tutorials 
                 </Link>
             </div>           
-            {/* <div className="right menu">
-                <GoogleAuth />
-            </div> */}
         </div>
     )
 }
