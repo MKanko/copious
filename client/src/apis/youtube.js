d0e2ff5a1da2bf226119b8edac0a84f47b856030
“Mark <mkanko66@gmail.com>
2020-09-21 21:06:04 -0700

    switch youtube keys as test to problem solve video loading

--- client/src/apis/youtube.js
+++ client/src/apis/youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const KEY = 'AIzaSyCqSZoBxSy8VN3SAeCjMC0KFPDj8gfxwo4'
-const KEY = 'AIzaSyC6pC_00rGGUplKufCs4QFQV7L8-EifWu4'
+const KEY = 'AIzaSyCqSZoBxSy8VN3SAeCjMC0KFPDj8gfxwo4'
+// const KEY = 'AIzaSyC6pC_00rGGUplKufCs4QFQV7L8-EifWu4'
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
